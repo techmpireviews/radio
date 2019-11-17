0404e9be2492f21d6b03fd8cf9934dea3884a30f
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-11-17 00:06:56 -0600

    Add files via upload

--- start.php
+++ start.php
@@ -3,11 +3,9 @@
 <head>
 	<title>Mobile</title>
 	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="manifest" href="/package.json">
-    <link rel="apple-touch-icon" sizes="512x512" href="images/goldicon.png">
     <link rel="icon" href="images/goldicon.png">
 </head>
 <body>
-<iframe frameborder="0px" style="border: none; position: absolute; left: 0px; top: 0px; width: 100%; height: 100%;" src="https://lintonradio.herokuapp.com"></iframe>
+<iframe frameborder="0px" style="border: none; position: absolute; left: 0px; top: 0px; width: 100%; height: 90%;" src="https://lintonradio.herokuapp.com"></iframe>
 </body>
 </html>
