9d1d3e5ceaaf7855d4b5a3004e01145c0ca11805
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-11-17 00:08:32 -0600

    Add files via upload

--- start.php
+++ start.php
@@ -6,6 +6,6 @@
     <link rel="icon" href="images/goldicon.png">
 </head>
 <body>
-<iframe frameborder="0px" style="border: none; position: absolute; left: 0px; top: 0px; width: 100%; height: 90%;" src="https://lintonradio.herokuapp.com"></iframe>
+<iframe frameborder="0px" style="border: none; position: absolute; left: 0px; top: 0px; width: 100%; height: 100%;" src="https://lintonradio.herokuapp.com"></iframe>
 </body>
 </html>
