7033d5dd70edbef8d4d7b9b3028afdf19ac359d6
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-11-17 00:02:35 -0600

    Add files via upload

--- start.php
+++ start.php
@@ -4,6 +4,6 @@
 	<title>Mobile</title>
 </head>
 <body>
-<iframe frameborder="0px" style="border: none; position: absolute; left: 0px; top: 0px;" src="https://lintonradio.herokuapp.com"></iframe>
+<iframe frameborder="0px" style="border: none; position: absolute; left: 0px; top: 0px; width: 100%; height: 100%;" src="https://lintonradio.herokuapp.com"></iframe>
 </body>
 </html>
