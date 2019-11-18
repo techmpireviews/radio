00a217c9faf515aee3e8558bcfbb40f2bf749137
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-11-17 18:01:45 -0600

    Update home.php

--- home.php
+++ home.php
@@ -234,11 +234,11 @@ position: absolute; left: 50%; transform: translate(-50%); font-size: 38px; font
 		border-color: transparent;
 	}
 	#klhsicons {
-		width: 120px;
+		width: auto;
 		margin-left: 3px;
 	}
 	#klpsicons {
-		width: 120px;
+		width: auto;
 		margin-left: 3px;
 	}
 </style>
