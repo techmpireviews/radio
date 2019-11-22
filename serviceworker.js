2e50ed27910b73882a8dd2da79c0000cdd1ad6b1
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-11-22 00:05:18 -0600

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-/* self.addEventListener('install', evt => {
+self.addEventListener('install', evt => {
 	console.log('service worker installed');
 });
 
@@ -8,36 +8,4 @@ self.addEventListener('activate', evt => {
 
 self.addEventListener('fetch', evt => {
 	console.log('fetch event', evt);
-}); */
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  ...
-});
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  showInstallPromotion();
-});
-
-btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
 });
