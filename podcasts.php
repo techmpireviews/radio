1bdb5ae340efd552debca797f45e4fe897c49dd8
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-11-21 23:16:58 -0600

    Add files via upload

--- podcasts.php
+++ podcasts.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<script src="serviceworker.js"></script>
 	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="manifest" href="manifest.json">
     <link rel="apple-touch-icon" sizes="512x512" href="images/goldicon.png">
     <link rel="icon" href="images/goldicon.png">
-	<title>Linton Radio App</title>
+	<title>Get Linton Radio App</title>
 </head>
 <body>
 <center><h1>Add this web app to your home screen!
